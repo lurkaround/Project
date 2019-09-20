8d463d48f9fdffe1bc59db8e822c9762308e41dd
lurkaround <rhelon@gmail.com>
2019-09-20 15:13:39 +0200

    created shared folder and updated shopping-list component

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Ingredient, ingredient } from '../shared/ingredient.model'
+
 @Component({
-  selector: 'app-shopping-list',
-  templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  selector: "app-shopping-list",
+  templateUrl: "./shopping-list.component.html",
+  styleUrls: ["./shopping-list.component.css"]
 })
 export class ShoppingListComponent implements OnInit {
+  ingredients: Ingredient[] = [
+    new ingredient("Apples", 5),
+    new ingredient("Tomatoes", 10),
+  ];
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
